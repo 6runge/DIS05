43722c1b6cc545b2286347b95d6b62c111fa0cf1
6runge <28950411+6runge@users.noreply.github.com>
2018-06-09 19:41:21 +0200

    [TASK] setup structure

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,4 @@
 
-public class Client {
+public class Client extends Thread{
 
 }
--- src/PersistenceManager.java
+++ src/PersistenceManager.java
@@ -1,4 +1,25 @@
+import java.util.Hashtable;
 
 public class PersistenceManager {
 
+	static final private PersistenceManager instance;
+	
+	private Hashtable buffer;
+	
+	static {
+		try {
+			instance = new PersistenceManager();
+		}
+		catch (Throwable e) {
+			throw new RuntimeException(e.getMessage());
+		}
+	}
+	
+	private PersistenceManager() {
+		buffer = new Hashtable();
+	}
+	
+	static public PersistenceManager getInstance() {
+		return instance;
+	}
 }
